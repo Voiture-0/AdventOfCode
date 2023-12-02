f179f3870d6162005d8da4d6f13afd69af1586e6
Voiture <TheRealVoiture@gmail.com>
2023-12-02 05:55:15 -0500

    day 2 part 2 initial answer

--- AdventOfCode2023/Day2.cs
+++ AdventOfCode2023/Day2.cs
@@ -5,50 +5,52 @@
 /// </summary>
 public class Day2
 {
+    private class Game
+    {
+        public int Id { get; set; }
+        public int Red { get; set; } = 0;
+        public int Blue { get; set; } = 0;
+        public int Green { get; set; } = 0;
+        public Game(string input)
+        {
+            var colonSplit = input.Split(':');
+            Id = int.Parse(colonSplit[0].Split(' ')[1]);
+            var sets = colonSplit[1].Split(';');
+            foreach (var set in sets)
+            {
+                var cubes = set.Split(',');
+                foreach (var c in cubes)
+                {
+                    var data = c.Split(' ');
+                    var count = int.Parse(data[1]);
+                    var color = data[2];
+                    switch (color)
+                    {
+                        case "red":
+                            Red = Math.Max(Red, count);
+                            break;
+                        case "blue":
+                            Blue = Math.Max(Blue, count);
+                            break;
+                        case "green":
+                            Green = Math.Max(Green, count);
+                            break;
+                    }
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Game {Id}: {Red} red, {Blue} blue, {Green} green";
+        }
+    }
+
     public class Part1(string[] input)
     {
         const int MaxRed = 12;
         const int MaxGreen = 13;
         const int MaxBlue = 14;
-        private class Game
-        {
-            public int Id { get; set; }
-            public int Red { get; set; } = 0;
-            public int Blue { get; set; } = 0;
-            public int Green { get; set; } = 0;
-            public Game(string input)
-            {
-                var colonSplit = input.Split(':');
-                Id = int.Parse(colonSplit[0].Split(' ')[1]);
-                var sets = colonSplit[1].Split(';');
-                foreach(var set in sets)
-                {
-                    var cubes = set.Split(',');
-                    foreach (var c in cubes)
-                    {
-                        var data = c.Split(' ');
-                        var count = int.Parse(data[1]);
-                        var color = data[2];
-                        switch (color)
-                        {
-                            case "red":
-                                Red = Math.Max(Red, count);
-                                break;
-                            case "blue":
-                                Blue = Math.Max(Blue, count);
-                                break;
-                            case "green":
-                                Green = Math.Max(Green, count);
-                                break;
-                        }
-                    }
-                }
-            }
-            public override string ToString()
-            {
-                return $"Game {Id}: {Red} red, {Blue} blue, {Green} green";
-            }
-        }
+        
         public string Run()
         {
             var sum = 0;
@@ -70,7 +72,14 @@ public class Day2
     {
         public string Run()
         {
-            return string.Empty;
+            var sum = 0;
+            foreach (var line in input)
+            {
+                var game = new Game(line);
+                var power = game.Red * game.Green * game.Blue;
+                sum += power;
+            }
+            return sum.ToString();
         }
     }
 }
