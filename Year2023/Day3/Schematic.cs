0fe91fe3aa2d89ffb83bf9a4be2370f502cd3b14
Voiture <TheRealVoiture@gmail.com>
2023-12-04 10:25:23 -0500

    fixed formatting

--- Year2023/Day3/Schematic.cs
+++ Year2023/Day3/Schematic.cs
@@ -6,11 +6,13 @@ public class Schematic
     {
         var values = new List<int>();
         for (var y = 0; y < input.Length; ++y)
-        for (var x = 0; x < input[y].Length; ++x)
         {
-            if (!partSymbols.Contains(input[y][x])) continue;
-            var partNumbers = GetPartNumbers(input, x, y);
-            values.Add(processPart(partNumbers));
+            for (var x = 0; x < input[y].Length; ++x)
+            {
+                if (!partSymbols.Contains(input[y][x])) continue;
+                var partNumbers = GetPartNumbers(input, x, y);
+                values.Add(processPart(partNumbers));
+            }
         }
         return values;
     }
@@ -20,17 +22,20 @@ public class Schematic
         var partNumbers = new List<int>();
         // Find adjacent numbers
         for (var y = cY-1; y <= cY+1; ++y)
-        for (var x = cX-1; x <= cX+1; ++x)
         {
-            if (!char.IsDigit(input[y][x])) continue;
-            var number = "";
-            // Check to the left until start of number
-            while (--x >= 0 && char.IsDigit(input[y][x]));
-            // Concatenate digits from left to right until end of number
-            while (++x < input[y].Length && char.IsDigit(input[y][x])) number += input[y][x];
-            // Save adjacent "part" number
-            partNumbers.Add(int.Parse(number));
+            for (var x = cX-1; x <= cX+1; ++x)
+            {
+                if (!char.IsDigit(input[y][x])) continue;
+                var number = "";
+                // Check to the left until start of number
+                while (--x >= 0 && char.IsDigit(input[y][x]));
+                // Concatenate digits from left to right until end of number
+                while (++x < input[y].Length && char.IsDigit(input[y][x])) number += input[y][x];
+                // Save adjacent "part" number
+                partNumbers.Add(int.Parse(number));
+            }
         }
+
         return partNumbers;
     }
 }
